aad44dbf27b820a13faf7a10840dd410fcafad4f
peesrikaew <ndodth@gmail.com>
2023-10-09 00:41:15 +0700

    P : change file path in bar

--- src/CharacterBar.java
+++ src/CharacterBar.java
@@ -1,4 +1,3 @@
-package javaapplication1;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,9 +11,9 @@ public class CharacterBar extends JFrame implements ActionListener {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Load images of character buttons (replace with actual image paths)
-        ImageIcon adam = new ImageIcon("Adam-1.png");
-        ImageIcon tree = new ImageIcon("tree.png");
-        ImageIcon tank = new ImageIcon("tank.png");
+        ImageIcon adam = new ImageIcon("../MvM-Game/pic/Hero/Adam/Adam-1.png");
+        ImageIcon ben = new ImageIcon("../MvM-Game/pic/Hero/Adam/Ben-1.png");
+        ImageIcon cat = new ImageIcon("../MvM-Game/pic/Hero/Adam/Cat-1.png");
         ImageIcon tankman = new ImageIcon("StickTankMan.png");
 
         characterButtons = new JButton[4];
@@ -22,13 +21,12 @@ public class CharacterBar extends JFrame implements ActionListener {
         // Create buttons for each character
         for (int i = 0; i < characterButtons.length; i++) {
             characterButtons[i] = new JButton();
-            ImageIcon icon = (i == 0 ? adam : (i == 1 ? tree : (i == 2 ? tank : tankman)));
+            ImageIcon icon = (i == 0 ? adam : (i == 1 ? ben : (i == 2 ? cat : tankman)));
             characterButtons[i].setIcon(getScaledImageIcon(icon, 100, 100));
             characterButtons[i].setPreferredSize(new Dimension(100, 150)); // Set the preferred size for the buttons
             characterButtons[i].setActionCommand(Integer.toString(i));
             characterButtons[i].addActionListener(this);
 
-            // Create a JLabel for the text and set its position
             String labelText = (i == 0 ? "20 points" : (i == 1 ? "10 points" : (i == 2 ? "20 points" : "40 points")));
             JLabel label = new JLabel(labelText, SwingConstants.CENTER);
             label.setVerticalAlignment(SwingConstants.BOTTOM); // Align text to the bottom
@@ -44,9 +42,9 @@ public class CharacterBar extends JFrame implements ActionListener {
         add(buttonPanel);
 
         // Set the size of the JFrame based on the button size
-        int frameWidth = characterButtons.length * 100 + 20; // Add 20 for padding
-        int frameHeight = 200; // Set your desired height
-        setSize(frameWidth, frameHeight);
+        int frameWidth = characterButtons.length * 100 + 20; //
+        int frameHeight = 200; // r desired height
+        setSize(frameWidth,frameHeight);
 
         setLocationRelativeTo(null);
     }
