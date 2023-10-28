89caf260f7f47b8db9991a7c238abbf3d75762c9
Jirasit <48820052+TONG2K@users.noreply.github.com>
2023-10-28 17:02:05 +0700

    T: Re-Model

---
+++ src/com/zetcode/Main.java
@@ -0,0 +1,26 @@
+package com.zetcode;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+	public Main() {
+
+		initUI();
+	}
+	private void initUI() {
+		add(new Board());
+
+		setTitle("The Thief Escape");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(380, 420);
+		setLocationRelativeTo(null);
+	}
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(() -> {
+			var ex = new Main();
+			ex.setVisible(true);
+		});
+	}
+}
